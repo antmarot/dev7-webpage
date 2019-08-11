c5ab24cfed085406d9ec29b74f3a8c177487adc2
Antoine M <antmarot@gmail.com>
2019-08-11 16:57:35 +0200

    Updates

---
+++ precache-manifest.4cb0b7181fa761c42037abadaed748f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a451a100cb485af9f5d7fbb72624ae5d",
+    "url": "/dev7-webpage/index.html"
+  },
+  {
+    "revision": "df47dc9062a33251d642",
+    "url": "/dev7-webpage/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "817823568ddcdbe7831d",
+    "url": "/dev7-webpage/static/js/2.79b01dc4.chunk.js"
+  },
+  {
+    "revision": "df47dc9062a33251d642",
+    "url": "/dev7-webpage/static/js/main.ee4ec3e0.chunk.js"
+  },
+  {
+    "revision": "e1351647fa5918fad1c9",
+    "url": "/dev7-webpage/static/js/runtime~main.aac78d3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dev7-webpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
